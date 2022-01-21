fb3b5710bf93716dfda14ecdd42d2ca1e6be99d2
AleksiPamilo <aleksi.pamilo@live.com>
2022-01-21 09:32:55 +0200

    Fixed Landing Page Buttons

--- src/components/sites/LandingPage/Main.tsx
+++ src/components/sites/LandingPage/Main.tsx
@@ -14,8 +14,8 @@ const LandingPage: React.FC<LandingPageProps> = () => {
       <h1 className="name mt-2 text-5xl md:text-7xl">Aleksi Pamilo.</h1>
       <h2 className="secondary-txt text-3xl md:text-5xl">Welcome to my portfolio!</h2>
       <p className="secondary-txt mt-10 mb-10">I am currently studying information and communication technologies.<br />I have experience in JS, TS, C#, HTML, CSS and React.</p>
-      <Link to="/projects" className="py-3 px-4 rounded-lg border-2 text-cyan-500 border-cyan-500 mr-3 hover:text-white hover:bg-cyan-500 transition ease-in-out duration-500">Check out my projects!</Link>
-      <Link to="/cv" className="py-3 px-4 rounded-lg border-2 text-cyan-500 border-cyan-500 mr-3 hover:text-white hover:bg-cyan-500 transition ease-in-out duration-500">Check out my CV!</Link>
+      <Link to="/projects" className="inline-block py-3 px-4 w-[200px] text-center rounded-lg border-2 text-cyan-500 border-cyan-500 mr-3 mb-3 hover:text-white hover:bg-cyan-500 transition ease-in-out duration-500">Check out my projects!</Link>
+      <Link to="/cv" className="inline-block py-3 px-4 w-[200px] text-center rounded-lg border-2 text-cyan-500 border-cyan-500 mr-3 hover:text-white hover:bg-cyan-500 transition ease-in-out duration-500">Check out my CV!</Link>
     </div>
   )
 }
