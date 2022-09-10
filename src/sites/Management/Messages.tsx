4b57f2a8972f733e39f5915c711e352f0e2d3ceb
AleksiPamilo <aleksi.pamilo@live.com>
2022-09-10 16:38:37 +0300

    Add authContext, management sites, login..

--- src/sites/Management/Messages.tsx
+++ src/sites/Management/Messages.tsx
@@ -3,27 +3,21 @@ import { HiLogout } from "react-icons/hi";
 import { useAuthContext } from "../../components/context/authContextProvider";
 
 const Messages: React.FC = () => {
-    const { authIsLoading, isLoggedIn, checkLogin, handleLogout } = useAuthContext();
+    const { checkLogin, handleLogout } = useAuthContext();
 
     useEffect(() => {
         checkLogin();
     }, []);
 
-    if (authIsLoading) {
-        return <div className="pt-[20rem] w-full text-center">Loading...</div>
-    } else if (!isLoggedIn) {
-        return <div />
-    } else {
-        return (
-            <div className="flex w-full h-full pt-[10rem]">
-                <button onClick={handleLogout} className="absolute top-0 right-0 mt-3 mr-[15%] md:mr-[10%] bg-cyan-600 py-2 px-3 rounded-lg"><HiLogout className="w-5 h-5 text-white" /></button>
-                <div className="">
-                    <h1 className="text-4xl font-bold text-center">Messages</h1>
+    return (
+        <div className="flex w-full h-full pt-[10rem]">
+            <button onClick={handleLogout} className="absolute top-0 right-0 mt-3 mr-[15%] md:mr-[10%] bg-cyan-600 py-2 px-3 rounded-lg"><HiLogout className="w-5 h-5 text-white" /></button>
+            <div className="">
+                <h1 className="text-4xl font-bold text-center">Messages</h1>
 
-                </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Messages;
--- src/sites/Management/index.tsx
+++ src/sites/Management/index.tsx
@@ -2,10 +2,9 @@ import React, { useEffect } from "react";
 import { useAuthContext } from "../../components/context/authContextProvider";
 import { HiLogout } from "react-icons/hi"
 import BarChart from "../../components/Management/Charts/BarChart";
-import Login from "./Login";
 
 const Management: React.FC = () => {
-    const { isLoggedIn, authIsLoading, checkLogin, handleLogout } = useAuthContext();
+    const { checkLogin, handleLogout } = useAuthContext();
     useEffect(() => checkLogin(), []);
 
     return (
@@ -14,19 +13,6 @@ const Management: React.FC = () => {
             <BarChart />
         </div>
     )
-
-    if (authIsLoading) {
-        return <div className="pt-[20rem] w-full flex justify-center items-center">Loading...</div>
-    } else if (!isLoggedIn) {
-        return <Login />
-    } else {
-        return (
-            <div className="pt-24 flex justify-center items-center">
-                <button onClick={handleLogout} className="absolute top-0 right-0 mt-3 mr-[15%] md:mr-[10%] bg-cyan-600 py-2 px-3 rounded-lg"><HiLogout className="w-5 h-5 text-white" /></button>
-                <BarChart />
-            </div>
-        )
-    }
 }
 
 export default Management;
