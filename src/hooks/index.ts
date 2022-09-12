ae7def82fd552dd43f446b997fb4248245b1face
AleksiPamilo <aleksi.pamilo@live.com>
2022-09-12 16:42:22 +0300

    Update hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,7 @@
+import { useAuthContext as useAuth } from "../components/context/authContextProvider";
+import { useDarkmodeContext as useDarkmode } from "../components/context/darkmodeContextProvider";
+
+export {
+    useAuth,
+    useDarkmode,
+};
