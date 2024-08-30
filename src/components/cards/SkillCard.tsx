9774bafee92b2194e81aa597950e8158dafb0910
AleksiPamilo <aleksi.pamilo@live.com>
2024-08-30 12:04:12 +0300

    Fix skill card

--- src/components/cards/SkillCard.tsx
+++ src/components/cards/SkillCard.tsx
@@ -7,13 +7,13 @@ type SkillCardProps = {
 
 const SkillCard: React.FC<SkillCardProps> = ({ skill }) => {
     return (
-        <div className="flex flex-col max-w-[25rem] p-4 gap-6 rounded-md select-none shadow-xl border border-emerald-400">
-            <span className="flex items-center gap-2">
+        <div className="grid grid-rows-7 max-w-[25rem] h-[12.5rem] p-4 gap-6 rounded-md select-none shadow-xl border border-emerald-400">
+            <span className="flex items-center gap-2 row-span-2">
                 {skill?.icon}
                 <h1 className="uppercase font-yellowtail">{skill.name}</h1>
             </span>
 
-            <p>{skill?.desc}</p>
+            <p className="row-span-3">{skill?.desc}</p>
 
             {
                 skill.tags && (
